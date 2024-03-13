e362dc280f90dde684ffd3b2aca2d4fb68ca45b4
Eduardo7125 <149196233+Eduardo7125@users.noreply.github.com>
2024-03-13 19:41:35 +0100

    Add files via upload

---
+++ Pandemic/Pandemic_Eduardo_Marc/src/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+	private static String ciudad;
+	private static int[] coordenadas = new int[3];
+	private static String[] ciudades;
+	
+	public static void main(String[] args) {
+		
+		String ejemplo = "San Francisco;0;235,315;Chicago,Los Angeles,Manila,Tokio;";
+		
+	}
+}
