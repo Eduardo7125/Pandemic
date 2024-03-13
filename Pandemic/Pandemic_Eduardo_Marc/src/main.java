9153a5b92791a0515ef1108415f06f2ae1695603
MarcHunter87 <148896284+MarcHunter87@users.noreply.github.com>
2024-03-13 20:55:07 +0100

    Add files via upload

--- Pandemic/Pandemic_Eduardo_Marc/src/main.java
+++ Pandemic/Pandemic_Eduardo_Marc/src/main.java
@@ -1,43 +1,55 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class main {
-	private static Scanner scan = new Scanner(System.in);
-	private static String ciudad;
-	private static int[] coordenadas = new int[3];
-	private static String[] ciudades;
-	
-	public static void main(String[] args) {
-		
-		String valor = scan.nextLine();
-//		San Francisco;0;235,315;Chicago,Los Angeles,Manila,Tokio;
-		
-		String[] x = valor.split(";");
-		for (int i = 0; i < x.length; i++) {
-			if(i == 0) {
-				ciudad = x[i];
-			} else if(i == 1) {
-				coordenadas[0] = Integer.parseInt(x[i]);
-			}else if(i == 2) {
-				 String[] parts = x[i].split(",");
-	                coordenadas[1] = Integer.parseInt(parts[0]);
-	                coordenadas[2] = Integer.parseInt(parts[1]);
-			} else {
-				ciudades = x[i].split(",");
-			}
-		}
-		
-		String valor2 = "Silco actúa en " + ciudad +" con los números "
-		+ coordenadas[0] +", "+ coordenadas[1] +" y "+ coordenadas[2] + " y cuyas ciudades "
-				+ "colindantes son ";
-		
-		for (int i = 0; i < ciudades.length - 1; i++) {
-            valor2 += ciudades[i];
-            if (i < ciudades.length - 2) {
-                valor2 += ", ";
-            }
-        }
+    private static String ciudad;
+    private static int[] coordenadas = new int[3];
+    private static String[] ciudades;
+    private static String nombreFichero = "ciudades.txt";
 
-        valor2 += " y " + ciudades[ciudades.length - 1];
-        System.out.println(valor2);
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            FileReader fileReader = new FileReader(nombreFichero);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String valor;
+            while ((valor = bufferedReader.readLine()) != null) {
+                String[] x = valor.split(";");
+                for (int i = 0; i < x.length; i++) {
+                    if (i == 0) {
+                        ciudad = x[i];
+                    } else if (i == 1) {
+                        coordenadas[0] = Integer.parseInt(x[i]);
+                    } else if (i == 2) {
+                        String[] parts = x[i].split(",");
+                        coordenadas[1] = Integer.parseInt(parts[0]);
+                        coordenadas[2] = Integer.parseInt(parts[1]);
+                    } else {
+                        ciudades = x[i].split(",");
+                    }
+                }
+
+                String valor2 = "Silco actúa en " + ciudad + " con los números "
+                        + coordenadas[0] + ", " + coordenadas[1] + " y " + coordenadas[2] + " y cuyas ciudades "
+                        + "colindantes son ";
+
+                for (int i = 0; i < ciudades.length - 1; i++) {
+                    valor2 += ciudades[i];
+                    if (i < ciudades.length - 2) {
+                        valor2 += ", ";
+                    }
+                }
+
+                valor2 += " y " + ciudades[ciudades.length - 1];
+                System.out.println(valor2);
+                Thread.sleep(30);
+            }
+
+            bufferedReader.close();
+            fileReader.close();
+
+        } catch (IOException e) {
+            System.out.println("Ha habido un error al intentar abrir el fichero" + e);
         }
+    }
 }
