77064474a042f486ca31cf9b23fb6b058eaccc45
Eduardo7125 <eduardocristea55@gmail.com>
2024-03-19 00:28:21 +0100

    EL_PUTO_ARCHIVO
    EL PUTO ARCHIVO QUE NOS HAN DADO ESTA MAL ESCRITO Y ENCIMA HAY COSAS QUE NI APARECEN LLEVO 5 HORAS MIRANDO QUE TENIA MAL Y ERA PROBLEMA DE LO QUE NOS HAN DADO

--- Pandemic/Pandemic_Eduardo_Marc/src/Cap2_1.java
+++ Pandemic/Pandemic_Eduardo_Marc/src/Cap2_1.java
@@ -2,81 +2,72 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.List;
 import java.util.Map;
 
 public class Cap2_1 {
-	private static Dictionary<Integer, String> ciudad = new Hashtable<Integer, String>();
-	private static Dictionary<Integer, Integer> enfermedades = new Hashtable<Integer, Integer>();
-	private static Dictionary<Integer, Integer> cord1 = new Hashtable<Integer, Integer>();
-	private static Dictionary<Integer, Integer> cord2 = new Hashtable<Integer, Integer>();
-	Map<String, Integer> ciudadReverse = new HashMap<>();
+    private static Map<Integer, String> ciudad = new HashMap<>();
+    private static Map<String, Integer> ciudad_2 = new HashMap<>();
+    private static Map<Integer, Integer> enfermedades = new HashMap<>();
+    private static Map<Integer, Integer> cord1 = new HashMap<>();
+    private static Map<Integer, Integer> cord2 = new HashMap<>();
+    private static ArrayList<String[]> array = new ArrayList<String[]>();
     private static String[] ciudades;
-    private static int indice = 1;
+    private static int indice = 0;
+    private static double x1;
+    private static double y1;
+    private static double x2;
+    private static double y2;
+    private static double distancia;
+    private static String valor;
     private static String nombreFichero = "ciudades.txt";
     
 	public static void main(String[] args) {
 		try (FileReader fileReader = new FileReader(nombreFichero);
                 BufferedReader bufferedReader = new BufferedReader(fileReader);){
-			 	String valor;
-			 
 	            while ((valor = bufferedReader.readLine()) != null) {
 	                String[] x = valor.split(";");
-	                for (int i = 1; i < x.length+1; i++) {
-	                    if (i == 1) {
-	                    	
-	                    	ciudad.put(indice,x[0]);
-	                    	
-	                    } else if (i == 2) {
-	                    	
-	                    	enfermedades.put(indice,Integer.parseInt(x[1]));
-	                    	
-	                    } else if (i == 3) {
-	                    	
-	                        String[] parts = x[2].split(",");
-	                        cord1.put(indice,Integer.parseInt(parts[0]));
-	                        cord2.put(indice,Integer.parseInt(parts[1]));
-	                        
-	                    } else {
-	                    	
-	                        ciudades = x[i-1].split(",");
-	                        
-	                    }
-	                }
+           
+          
+                	ciudad.put(indice,x[0]);
+                	ciudad_2.put(x[0],indice);
+           
+                	
+                	enfermedades.put(indice,Integer.parseInt(x[1]));
+         
+                	
+                    String[] parts = x[2].split(",");
+                    cord1.put(indice,Integer.parseInt(parts[0]));
+                    cord2.put(indice,Integer.parseInt(parts[1]));
+                    
+                    
+                    ciudades = x[3].split(",");
+                	array.add(ciudades);
+                    
+
                 	indice++;
 	            }
-	            System.out.println(((Map<Integer, String>) cord1).keySet());
-	            int i = 1;
-	            while(ciudad.get(i) != null) {
-	        		int cordenada1 = (Integer)cord1.get(i) - (Integer)cord1.get(ciudades[i]);
-	        		int cordenada2 = (Integer)cord2.get(i) - (Integer)cord2.get(ciudades[i]);
-	            	int distancia_al_cuadrado = (cordenada1^2) + (cordenada2^2);
-	            	int distancia =	(int) Math.sqrt(distancia_al_cuadrado);  
-	                System.out.println("La ciudad "+ciudad.get(i)+" esta en las cordenadas ("+cord1.get(i)+","+cord2.get(i)+") sus ciudades colindantes son:");
-	                for (int j = 0; j < ciudades.length; j++) {
-	                    System.out.println(ciudades[j]+", que esta a una distancia de ");
-	        		}
-	            	i++;
+	            for (int cityIndex : ciudad.keySet()) {
+	                calculo(cityIndex);
 	            }
+
 		} catch (IOException e) {
 			System.out.println("Ha habido un error al intentar abrir el fichero" + e);
 		}
 	}
 	
-	public static void calculo() {
-//		(300, 540)-(200,605) = (100,65)
-//		int cordenada1 = cord1.get(i) - cord1.get(ciudades[i]);
-//		int cordenada2 = cord2.get(i) - cord2.get(ciudades[i]);
-//    	int distancia_al_cuadrado = (cordenada1^2) + (cordenada2^2);
-//    	int distancia =	(int) Math.sqrt(distancia_al_cuadrado);  
-//        String valor2 = "La ciudad "+ciudad+"esta en las cordenadas ("+cord1.get(i)+","+cord2.get(i)+") sus ciudades colindantes son:";
-//        for (int j = 0; i < ciudades.length; i++) {
-//            String valor3 = ciudades[j]+" que esta a una distancia de "+distancia;
-//		}
+	public static void calculo(int cityIndex) {
+    	x1 = cord1.get(cityIndex);
+        y1 = cord2.get(cityIndex);
+        ciudades = array.get(cityIndex);
+		System.out.println("La ciudad "+ciudad.get(cityIndex)+" esta en las cordenadas ("+cord1.get(cityIndex)+","+cord2.get(cityIndex)+") sus ciudades colindantes son:");
+        	for (String ciudadColindante : ciudades) {
+                x2 = cord1.get(ciudad_2.get(ciudadColindante));
+                y2 = cord2.get(ciudad_2.get(ciudadColindante));
+                distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+                System.out.println(ciudadColindante+", que esta a una distancia de "+distancia);
+			}
+        System.out.println();
 	}
-	
 }
 
