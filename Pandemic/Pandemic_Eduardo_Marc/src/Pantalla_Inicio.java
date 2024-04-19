f636edb728cee27ce10f81da72cd507640794f7b
Edu <eduardocristea55@gmail.com>
2024-04-19 19:33:28 +0200

    Arreglando el codigo espaguetti

--- Pandemic/Pandemic_Eduardo_Marc/src/Pantalla_Inicio.java
+++ Pandemic/Pandemic_Eduardo_Marc/src/Pantalla_Inicio.java
@@ -1,4 +1,5 @@
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,120 +10,41 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Pantalla_Inicio {
-    private static JLabel menuLabel = new JLabel();
-    private static JLabel opcionElegidaLabel = new JLabel();
-    private static Point mouseDownCompCoords;
-    private static Point currCoords;
+	public static void main(String[] args) {
+		Marco mimimarco = new Marco();	
+	}
+
+}
+
+class Marco extends JFrame{
     private static Point frame1Position;
-    private static JFrame frame = new JFrame();
-    private static JPanel panel = new JPanel();
-    private static Color currentColor = new Color(173, 216, 230);
-    private static boolean toWhite = true;
-    private static Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-    
-    public static void main(String[] args) {
-        cargarPantallaInicio(frame, panel);
-    }
-
-    public static void cargarPantallaInicio(final JFrame frame, JPanel panel) {
-        frame.setSize(300, 530);
-        frame.setResizable(false);
-        frame.setUndecorated(true);
-    	frame.addComponentListener(new ComponentAdapter() {
-    	    @Override
-    	    public void componentMoved(ComponentEvent e) {
-    	        frame1Position = frame.getLocation();
-    	    }
-    	});
-
-        panel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                Graphics2D g2d = (Graphics2D) g.create();
-                g2d.setColor(currentColor);
-                g2d.fillRect(0, 0, getWidth(), getHeight());
-                g2d.dispose();
-            }
-        };
-        panel.setLayout(null);
-        panel.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-
-        ImageIcon icono = new ImageIcon("icon.png");
-        Image imagen = icono.getImage();
-        Image imagenEscalada = imagen.getScaledInstance(80, 80, Image.SCALE_SMOOTH);
-        ImageIcon iconoEscalado = new ImageIcon(imagenEscalada);
-        JLabel imagenLabel = new JLabel(iconoEscalado);
-        imagenLabel.setBounds(110, 123, 80, 80);
-        panel.add(imagenLabel);
-
-        JLabel menuLabel = new JLabel("<html><div style='text-align: center;'><h1>PANDEMIC</h1>"
-                + "<h2>MENÚ PRINCIPAL</h2><br>");
-        menuLabel.setBounds(75, -20, 260, 200);
-        panel.add(menuLabel);
-
-        JButton nuevaPartidaButton = new JButton("Nueva Partida");
-        nuevaPartidaButton.setBounds(55, 220, 190, 30);
-        styleButton(nuevaPartidaButton);
-        panel.add(nuevaPartidaButton);
+    ImageIcon icono;
+	public Marco(){
+		setTitle("Pandemic");
+		icono = new ImageIcon("icon.png");
+        setIconImage(icono.getImage());
         
-
-        JButton cargarPartidaButton = new JButton("Cargar Partida");
-        cargarPartidaButton.setBounds(55, 260, 190, 30);
-        styleButton(cargarPartidaButton);
-        panel.add(cargarPartidaButton);
-
-        JButton informacionButton = new JButton("Información");
-        informacionButton.setBounds(55, 300, 190, 30);
-        informacionButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                mostrarSegundoFrame();
-			}
-		});
-        styleButton(informacionButton);
-        panel.add(informacionButton);
-
-        JButton resumenButton = new JButton("Resumen de puntuaciones");
-        resumenButton.setBounds(55, 340, 190, 30);
-        styleButton(resumenButton);
-        panel.add(resumenButton);
-
-        JButton autoresButton = new JButton("Autores");
-        autoresButton.setBounds(55, 380, 190, 30);
-        styleButton(autoresButton);
-        panel.add(autoresButton);
-
-        JButton versionButton = new JButton("Versión");
-        versionButton.setBounds(55, 420, 190, 30);
-        styleButton(versionButton);
-        panel.add(versionButton);
-        
-
-        JButton salirButton = new JButton("Salir");
-        salirButton.setBounds(55, 460, 190, 30);
-        styleButton(salirButton);
-        salirButton.addActionListener(new ActionListener() {
+        setSize(300, 560);
+        setResizable(false);
+        setUndecorated(true);
+        addComponentListener(new ComponentAdapter() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
+            public void componentMoved(ComponentEvent e) {
+                frame1Position = getLocation();
             }
         });
-        
-        panel.add(salirButton);
-        
-        moveWindow(frame);
-        
-        fondo(frame, panel);
-
-        frame.add(panel);
-        
-        frame.setVisible(true);
-    }
-    
-    public static void fondo(final JFrame frame, JPanel panel) {
+        Lamina1 lamina1 = new Lamina1();
+		add(lamina1);
+		
+		fondo();
+		moveWindow();
+		
+		setVisible(true);
+	}
+	
+	private static boolean toWhite = true;
+	private static Color currentColor = new Color(173, 216, 230);
+    public void fondo() {
         Timer timer = new Timer(40, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -143,79 +65,130 @@ public class Pantalla_Inicio {
                         toWhite = true;
                     }
                 }
-                panel.repaint();
+                repaint();
             }
         });
         timer.start();
     }
-    
-    public static void mostrarSegundoFrame() {
-        JFrame frame2 = new JFrame("Segundo Frame");
-        frame2.setSize(300, 530);
-        frame2.setResizable(false);
-        frame2.setUndecorated(true);
-        
-        frame2.setLocation(currCoords.x - mouseDownCompCoords.x, currCoords.y - mouseDownCompCoords.y);
-
-        JPanel contentPane = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                Graphics2D g2d = (Graphics2D) g.create();
-                g2d.setColor(currentColor);
-                g2d.fillRect(0, 0, getWidth(), getHeight());
-                g2d.dispose();
-            }
-        };
-        contentPane.setLayout(null);
-        contentPane.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-        
-        JLabel menuLabel = new JLabel("<html><div style='text-align: center;'><h1>PANDEMIC</h1>"
-                + "<h2>MENÚ PRINCIPAL</h2><br>");
-        menuLabel.setBounds(70, 0, 260, 200);
-        contentPane.add(menuLabel);
-
-        JButton volverButton = new JButton("Volver");
-        volverButton.setBounds(55, 190, 190, 30);
-        styleButton(volverButton);
-        volverButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame2.dispose();
-                frame.setVisible(true);
-            }
-        });
-        contentPane.add(volverButton);
-        
-        fondo(frame2, contentPane);
-        frame2.setContentPane(contentPane);
-        frame2.setVisible(true);
-        moveWindow(frame2);
+    public void paintComponent(Graphics g) {
+        super.paintComponents(g);
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setColor(currentColor);
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+        g2d.dispose();
     }
     
-    public static void moveWindow(JFrame frame) {
-    	frame.addMouseListener(new MouseAdapter() {
+	private static Point currCoords = new Point();
+    private static Point mouseDownCompCoords;
+	private static Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+	public void moveWindow() {
+    	addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
                 mouseDownCompCoords = e.getPoint();
             }
         });
 
-	    frame.addMouseMotionListener(new MouseMotionAdapter() {
+	    addMouseMotionListener(new MouseMotionAdapter() {
             public void mouseDragged(MouseEvent e) {
                 currCoords = e.getLocationOnScreen();
-                frame.setLocation(currCoords.x - mouseDownCompCoords.x, currCoords.y - mouseDownCompCoords.y);
+                setLocation(currCoords.x - mouseDownCompCoords.x, currCoords.y - mouseDownCompCoords.y);
             }
         });
 
-        frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
-
-        
+        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
     }
+	
+}
+
+class Lamina1 extends JPanel implements ActionListener{
+	
+	ImageIcon icono;
+	ImageIcon iconoEscalado;
+	Image imagen;
+	Image imagenEscalada;
+
+	JPanel bottomPanel;
+	
+	JLabel version;
+	JLabel menuLabel1;
+	JLabel menuLabel2;
+	JLabel imagenLabel;
+	
+	JButton nuevaPartidaButton;
+	JButton cargarPartidaButton;
+	JButton informacionButton;
+	JButton resumenButton;
+	JButton autoresButton;
+	JButton salirButton;
+	
+	Lamina1(){
+		
+        icono = new ImageIcon("icon.png");
+        imagen = icono.getImage();
+        
+        imagenEscalada = imagen.getScaledInstance(80, 80, Image.SCALE_SMOOTH);
+        iconoEscalado = new ImageIcon(imagenEscalada);
+        imagenLabel = new JLabel(iconoEscalado);
+	
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(7, 7, 7, 7);
+		
+        menuLabel1 = new JLabel("<html><div style'text-align:center;'><h1>PANDEMIC</h1>");
+        menuLabel2 = new JLabel("<html><div style'text-align:center;'><h2>MENÚ PRINCIPAL</h2>");
+		nuevaPartidaButton = new JButton("Nueva Partida");
+		cargarPartidaButton = new JButton("Cargar Partida");
+		informacionButton = new JButton("Información");
+		resumenButton = new JButton("Resumen de puntuaciones");
+		autoresButton = new JButton("Autores");
+		salirButton = new JButton("Salir");
+		bottomPanel = new JPanel();
+		version = new JLabel("Version 1.0");
+		
+		styleButton(nuevaPartidaButton);
+		styleButton(cargarPartidaButton);
+		styleButton(informacionButton);
+		styleButton(resumenButton);
+		styleButton(autoresButton);
+		styleButton(salirButton);
+		
+		nuevaPartidaButton.addActionListener(this);
+		cargarPartidaButton.addActionListener(this);
+		informacionButton.addActionListener(this);
+		resumenButton.addActionListener(this);
+		autoresButton.addActionListener(this);
+		salirButton.addActionListener(this);
+		
+        add(menuLabel1, BorderLayout.NORTH);
+        gbc.gridy++;
+        add(menuLabel2, BorderLayout.NORTH);
+        gbc.gridy++;
+        add(imagenLabel, BorderLayout.CENTER);
+        gbc.gridy++;
+		add(nuevaPartidaButton, BorderLayout.CENTER);
+		gbc.gridy++;
+		add(cargarPartidaButton, BorderLayout.CENTER);
+		gbc.gridy++;
+		add(informacionButton, BorderLayout.CENTER);
+		gbc.gridy++;
+		add(resumenButton, BorderLayout.CENTER);
+		gbc.gridy++;
+		add(autoresButton, BorderLayout.CENTER);
+		gbc.gridy++;
+		add(salirButton, BorderLayout.CENTER);
+		bottomPanel.add(version);
+	    add(bottomPanel, BorderLayout.SOUTH);
+		
+	    setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+	   
+	}
     
-    private static void styleButton(JButton button) {
+    public static void styleButton(JButton button) {
         button.setBackground(new Color(240, 240, 240));
         button.setForeground(Color.BLACK);
         button.setFocusPainted(false);
+        button.setPreferredSize(new Dimension(200, 40));
         button.setFont(new Font("Arial", Font.BOLD, 12));
         button.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
         button.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -228,6 +201,26 @@ public class Pantalla_Inicio {
             }
         });
     }
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if (e.getSource() == nuevaPartidaButton) {
+			System.exit(0);
+		} else if (e.getSource() == cargarPartidaButton) {
+			System.exit(0);
+		} else if (e.getSource() == informacionButton) {
+//			lamina2.area1.setText("Has pulsado el bot�n Este");
+		} else if (e.getSource() == resumenButton) {
+			System.exit(0);
+		} else if (e.getSource() == autoresButton) {
+			System.exit(0);
+		}else if (e.getSource() == salirButton) {
+			System.exit(0);
+		}
+		
+	}
+	
     
 	public static void cargarRecords() {
 		
