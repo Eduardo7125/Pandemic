0fe9f9217481a685a92efa5e41c555d7ef34ad47
MarcHunter87 <148896284+MarcHunter87@users.noreply.github.com>
2024-05-02 20:19:47 +0200

    Add files via upload

---
+++ Pandemic_Eduardo_Marc/src/main/Pantalla_Inicio.java
@@ -0,0 +1,73 @@
+package main;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.plaf.basic.BasicProgressBarUI;
+
+import data_managment.Control_de_datos;
+
+import java.awt.*;
+import java.io.Serial;
+
+public class Pantalla_Inicio {
+	public static void main(String[] args) {
+	    Thread cargaPartidaThread = new Thread(() -> Control_de_datos.cargarPartida());
+	    cargaPartidaThread.start();
+
+	    SwingUtilities.invokeLater(() -> {
+	        Marco mimimarco = new Marco();
+	        mimimarco.setVisible(true);
+	    });
+	}
+
+}
+
+class Marco extends JFrame{
+    /**
+	 * 
+	 */
+	@Serial
+    private static final long serialVersionUID = 1L;
+	public Marco(){
+		setTitle("Pandemic");
+        setIconImage(new ImageIcon("src//img//icon.png").getImage());
+        GraphicsDevice graphicsDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        if (graphicsDevice.isFullScreenSupported()) {
+            setUndecorated(true);
+            setResizable(false);
+            graphicsDevice.setFullScreenWindow(this);
+        } else {
+            System.out.println("El modo de pantalla completa no es soportado por este dispositivo.");
+        }
+
+        menu menu = main.menu.getInstance();
+        
+        menu.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        add(menu);
+
+        setVisible(true);
+	}
+	
+}
+
+class CustomProgressBarUI extends BasicProgressBarUI {
+    private Image backgroundImage;
+
+    public CustomProgressBarUI(Image backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
+
+    @Override
+    protected void paintDeterminate(Graphics g, JComponent c) {
+        super.paintDeterminate(g, c);
+
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.drawImage(backgroundImage, 0, 0, c.getWidth(), c.getHeight(), null);
+
+        g2d.dispose();
+    }
+}
---
+++ Pandemic_Eduardo_Marc/src/objects/Vacunas.java
@@ -0,0 +1,57 @@
+package objects;
+
+public class Vacunas {
+
+	private String nombre;
+	private String color;
+	private float porcentaje;
+
+	public Vacunas(String nombre, String color, float porcentaje) {
+		this.nombre = nombre;
+		this.color = color;
+		this.porcentaje = porcentaje;
+	}
+	
+	boolean completo;
+	public void desarrollarVacuna(float porcentaje) {
+		completo = false;
+		int nuevoValor = (int) (this.getPorcentaje() + (int) porcentaje);
+		if (nuevoValor <= 100) {
+			this.setPorcentaje(nuevoValor);
+			System.out.println("Se aumento el valor de la vacuna " + this.getNombre() + " a " + nuevoValor);
+		} else {
+			completo = true;
+			System.out.println("La vacuna " + this.getNombre() + " ya esta completamente desarrollada.");
+		}
+
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public float getPorcentaje() {
+		return porcentaje;
+	}
+	
+	public void setPorcentaje(float porcentaje) {
+		this.porcentaje = porcentaje;
+	}
+
+	public boolean isCompleto() {
+		return completo;
+	}
+	
+}
