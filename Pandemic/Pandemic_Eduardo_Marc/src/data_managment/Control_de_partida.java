0127af798c65d468e0a739b15f7d7c0886c2ab60
MarcHunter87 <148896284+MarcHunter87@users.noreply.github.com>
2024-05-05 21:13:59 +0200

    Add files via upload

--- Pandemic/Pandemic_Eduardo_Marc/src/data_managment/Control_de_partida.java
+++ Pandemic/Pandemic_Eduardo_Marc/src/data_managment/Control_de_partida.java
@@ -1,5 +1,11 @@
 package data_managment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+import objects.Ciudad;
+
 public class Control_de_partida {
 
 	public static int turno = 1;
@@ -20,9 +26,37 @@ public class Control_de_partida {
 		
 	}
 	
-	public void gestionarInfeccion() {
-		
-	}
+	public static void gestionarInfeccion() {
+        // Accede a la lista de ciudades desde Control_de_datos
+        ArrayList<Ciudad> ciudades = Control_de_datos.Ciudades;
+
+        // Selecciona aleatoriamente cuatro ciudades
+        ArrayList<Ciudad> ciudadesAleatorias = seleccionarCuatroCiudades(ciudades);
+
+        // Ejecuta el método aumentarInfeccion() en cada una de las cuatro ciudades seleccionadas
+        for (Ciudad ciudad : ciudadesAleatorias) {
+            ciudad.aumentarInfeccion();
+            System.out.println("Name: " + ciudad.getNombre());
+            System.out.println("Virus: " + ciudad.getNombreEnfermedad());
+            System.out.println("Infection: " + ciudad.getInfeccion());
+            System.out.println();
+        }
+    }
+
+    // Método para seleccionar aleatoriamente cuatro ciudades de la lista
+    private static ArrayList<Ciudad> seleccionarCuatroCiudades(ArrayList<Ciudad> ciudades) {
+        ArrayList<Ciudad> ciudadesAleatorias = new ArrayList<>();
+        Random rand = new Random();
+    	int infect = Integer.parseInt(Control_de_datos.CiudadesInfectadasRonda);
+        // Selecciona cuatro índices únicos aleatorios
+        for (int i = 0; i < infect; i++) {
+            int index = rand.nextInt(ciudades.size());
+            Ciudad ciudad = ciudades.get(index);
+            ciudadesAleatorias.add(ciudad);
+        }
+
+        return ciudadesAleatorias;
+    }
 	
 	public void gestionarBrote() {
 		
