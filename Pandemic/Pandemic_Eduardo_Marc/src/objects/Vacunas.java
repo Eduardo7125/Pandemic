74f9e2221c6d2d3f3dad34cc4390d07c36a5eeea
MarcHunter87 <148896284+MarcHunter87@users.noreply.github.com>
2024-05-13 23:38:51 +0200

    Add files via upload

--- Pandemic/Pandemic_Eduardo_Marc/src/objects/Vacunas.java
+++ Pandemic/Pandemic_Eduardo_Marc/src/objects/Vacunas.java
@@ -1,58 +1,106 @@
 package objects;
 
+/**
+ * En esta clase se gestionan las distintas variables que afectan a las Vacunas.
+ * @author Eduardo y Marc
+ */
 public class Vacunas {
 
-	private String nombre;
-	private String color;
-	private float porcentaje;
+    private String nombre;
+    private String color;
+    private float porcentaje;
+    private boolean completo;
 
-	public Vacunas(String nombre, String color, float porcentaje) {
-		this.nombre = nombre;
-		this.color = color;
-		this.porcentaje = porcentaje;	
-	}
-	
-	boolean completo;
-	public void desarrollarVacuna(float porcentaje) {
-		
-			completo = false;
-			int nuevoValor = (int) (this.getPorcentaje() + (int) porcentaje);
-			if (nuevoValor <= 100) {
-				this.setPorcentaje(nuevoValor);
-				System.out.println("The value of vaccine " + this.getNombre() + " has been increased to " + nuevoValor);
-			} else {
-				completo = true;
-				System.out.println("Vaccine " + this.getNombre() + " is already fully developed.");
-			}	
-		
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	public String getColor() {
-		return color;
-	}
-	
-	public void setColor(String color) {
-		this.color = color;
-	}
-	
-	public float getPorcentaje() {
-		return porcentaje;
-	}
-	
-	public void setPorcentaje(float porcentaje) {
-		this.porcentaje = porcentaje;
-	}
+    /**
+     * Constructor para las vacunas.
+     * 
+     * @param nombre     Nombre de la vacuna.
+     * @param color      Color de la vacuna.
+     * @param porcentaje Porcentaje de desarrollo de la vacuna.
+     */
+    public Vacunas(String nombre, String color, float porcentaje) {
+        this.nombre = nombre;
+        this.color = color;
+        this.porcentaje = porcentaje;
+    }
 
-	public boolean isCompleto() {
-		return completo;
-	}
-	
+    /**
+     * Desarrollo de la vacuna (aumentar su porcentaje de desarrollo).
+     * 
+     * @param porcentajeIncremento Porcentaje que aumenta el desarrollo de la vacuna.
+     */
+    public void desarrollarVacuna(float porcentajeIncremento) {
+        completo = false;
+        int nuevoValor = (int) (this.getPorcentaje() + porcentajeIncremento);
+        if (nuevoValor <= 100) {
+            this.setPorcentaje(nuevoValor);
+            System.out.println("El valor de la vacuna " + this.getNombre() + " se ha aumentado a " + nuevoValor);
+        } else {
+            completo = true;
+            System.out.println("La vacuna " + this.getNombre() + " ya está completamente desarrollada.");
+        }
+    }
+
+    /**
+     * Obtener el nombre de la vacuna.
+     * 
+     * @return Nombre de la vacuna.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Establecer el nombre de la vacuna.
+     * 
+     * @param nombre Nuevo nombre de la vacuna.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Obtener el color de la vacuna.
+     * 
+     * @return Color de la vacuna.
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * Establecer el color de la vacuna.
+     * 
+     * @param color Nuevo color de la vacuna.
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * Obtener el porcentaje de desarrollo de la vacuna.
+     * 
+     * @return Porcentaje de desarrollo de la vacuna.
+     */
+    public float getPorcentaje() {
+        return porcentaje;
+    }
+
+    /**
+     * Establecer el porcentaje de desarrollo de la vacuna.
+     * 
+     * @param porcentaje Nuevo porcentaje de desarrollo de la vacuna.
+     */
+    public void setPorcentaje(float porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
+    /**
+     * Verificar si la vacuna está completamente desarrollada.
+     * 
+     * @return true si la vacuna está completa, false si no está completa.
+     */
+    public boolean isCompleto() {
+        return completo;
+    }
 }
--- Pandemic/Pandemic_Eduardo_Marc/src/objects/Virus.java
+++ Pandemic/Pandemic_Eduardo_Marc/src/objects/Virus.java
@@ -1,39 +1,79 @@
 package objects;
 
+/**
+ * En esta clase se gestionan las distintas variables que afectan a los virus.
+ * @author Eduardo y Marc
+ */
 public class Virus {
 
-	private String identificador;
-	private String nombre;
-	private String color;
-	
-	public Virus(String identificador, String nombre, String color) {
-		this.identificador = identificador;
-		this.nombre = nombre;
-		this.color = color;
-	}
-	
-	public String getIdentificador() {
-		return identificador;
-	}
-	
-	public void setIdentificador(String identificador) {
-		this.identificador = identificador;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	public String getColor() {
-		return color;
-	}
-	
-	public void setColor(String color) {
-		this.color = color;
-	}
-	
+    private String identificador;
+    private String nombre;
+    private String color;
+
+    /**
+     * Constructor para los Virus.
+     * 
+     * @param identificador Identificador del virus.
+     * @param nombre       Nombre del virus.
+     * @param color        Color del virus.
+     */
+    public Virus(String identificador, String nombre, String color) {
+        this.identificador = identificador;
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    /**
+     * Obtener el identificador del virus.
+     * 
+     * @return Identificador del virus.
+     */
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    /**
+     * Establecer el identificador del virus.
+     * 
+     * @param identificador Nuevo identificador del virus.
+     */
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    /**
+     * Obtener el nombre del virus.
+     * 
+     * @return Nombre del virus.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Establecer el nombre del virus.
+     * 
+     * @param nombre Nuevo nombre del virus.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Obteener el color del virus.
+     * 
+     * @return Color del virus.
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * Establecer el color del virus.
+     * 
+     * @param color Nuevo color del virus.
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
