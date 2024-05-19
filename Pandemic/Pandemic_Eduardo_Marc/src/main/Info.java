99e75622d5e3d507e271e5684c1ee957ad6ff535
MarcHunter87 <148896284+MarcHunter87@users.noreply.github.com>
2024-05-19 13:45:06 +0200

    Update Info.java

--- Pandemic/Pandemic_Eduardo_Marc/src/main/Info.java
+++ Pandemic/Pandemic_Eduardo_Marc/src/main/Info.java
@@ -4,8 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
